6cff176b130aa8aa7dcdf17c5369d8b15d1903f5
Leorasaharia <143778430+Leorasaharia@users.noreply.github.com>
2024-03-26 01:23:56 +0530

    first commit

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 const dynamicText = document.querySelector("h1 span");
-const words = ["Leora Saharia", "A Web Developer", "A Youtuber"];
+const words = ["Leora Saharia", "A Web Developer", "A Singer", "A Youtuber"];
 let wordIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
-
 const typeEffect = () => {
     const currentWord = words[wordIndex];
     const currentChar = currentWord.substring(0, charIndex);
@@ -23,6 +22,41 @@ const typeEffect = () => {
         setTimeout(typeEffect, 1200);
     }
 }
-
 typeEffect();
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})
+();
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.pageX;
+  const y = e.pageY;
+  const centerX = window.innerWidth / 2;
+  const centerY = window.innerHeight / 2;
+
+  const distanceX = x - centerX;
+  const distanceY = y - centerY;
+  const angle = Math.atan2(distanceY, distanceX);
+  
+  const radius = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+  
+  const swirlX = centerX + Math.cos(angle) * radius;
+  const swirlY = centerY + Math.sin(angle) * radius;
+  
+  cursor.style.left = swirlX + 'px';
+  cursor.style.top = swirlY + 'px';
+});
+
+
 
